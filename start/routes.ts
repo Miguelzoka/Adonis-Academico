d7d2ae385b60db8edb01a483785e3abcd933a631
Miguel Lima <miguel.cavalcanti@iesb.edu.br>
2022-10-20 23:09:58 -0300

    Atualizando rotas

--- start/routes.ts
+++ start/routes.ts
@@ -6,3 +6,11 @@ Route.get('/', async () => {
 
 Route.resource('/cursos', 'CursosController').apiOnly()
 Route.resource('/disciplinas', 'DisciplinasController').apiOnly()
+Route.resource('/alunos', 'AlunosController').apiOnly()
+Route.resource('/aulas', 'AulasController').apiOnly()
+Route.resource('/chamadas', 'ChamadasController').apiOnly()
+Route.resource('/professores', 'ProfessoresController').apiOnly()
+Route.resource('/salas', 'SalasController').apiOnly()
+Route.resource('/semestres', 'SemestresController').apiOnly()
+Route.resource('/turmas', 'TurmasController').apiOnly()
+Route.resource('/turmasalunos', 'TurmasAlunosController').apiOnly()
