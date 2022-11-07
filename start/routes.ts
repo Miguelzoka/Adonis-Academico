030f4849e37a58cac9f0a875cd2d9607f2feedea
Miguel Lima <miguel.cavalcanti@iesb.edu.br>
2022-11-07 20:48:52 -0300

    Criando Autenticacoes

--- start/routes.ts
+++ start/routes.ts
@@ -1,16 +1,20 @@
-import Route from '@ioc:Adonis/Core/Route'
+import Route from "@ioc:Adonis/Core/Route"
 
-Route.get('/', async () => {
-  return { hello: 'world' }
-})
+Route.get("/", async () => {
+  return { hello: "world" }
+});
 
-Route.resource('/cursos', 'CursosController').apiOnly()
-Route.resource('/disciplinas', 'DisciplinasController').apiOnly()
-Route.resource('/alunos', 'AlunosController').apiOnly()
-Route.resource('/aulas', 'AulasController').apiOnly()
-Route.resource('/chamadas', 'ChamadasController').apiOnly()
-Route.resource('/professores', 'ProfessoresController').apiOnly()
-Route.resource('/salas', 'SalasController').apiOnly()
-Route.resource('/semestres', 'SemestresController').apiOnly()
-Route.resource('/turmas', 'TurmasController').apiOnly()
-Route.resource('/turmasalunos', 'TurmasAlunosController').apiOnly()
+Route.post("/users", "UsersController.store")
+
+Route.group(() => {
+  Route.resource("/cursos", "CursosController").apiOnly()
+  Route.resource("/disciplinas", "DisciplinasController").apiOnly()
+  Route.resource("/alunos", "AlunosController").apiOnly()
+  Route.resource("/aulas", "AulasController").apiOnly()
+  Route.resource("/chamadas", "ChamadasController").apiOnly()
+  Route.resource("/professores", "ProfessoresController").apiOnly()
+  Route.resource("/salas", "SalasController").apiOnly()
+  Route.resource("/semestres", "SemestresController").apiOnly()
+  Route.resource("/turmas", "TurmasController").apiOnly()
+  Route.resource("/turmasalunos", "TurmasAlunosController").apiOnly()
+}).middleware("auth")
